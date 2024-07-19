3a9d3e8132646a0e921b72e8e15a61cb88661643
Matvey991 <matveyfedotov61@gmail.com>
2024-07-20 01:03:04 +0400

    react-app

--- src/App.js
+++ src/App.js
@@ -1,69 +1,19 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
-// import ClassCounter from "./componets/ClasssCounter";
-import './styles/App.css';
-import Postitem from "./componets/Postitem";
-import Postlist from "./componets/Postlist";
-import MyButton from "./componets/UI/button/MyButton";
-import MyInput from "./componets/UI/button/input/MyInput";
-import PostForm from "./componets/PostForm";
-import MySelect from "./componets/UI/select/MySelect";
-import PostFilter from "./componets/PostFilter";
-import MyModal from "./componets/Mymodal/MyModal";
-import { usePosts } from "./hooks/UsePosts";
-import UusePosts from './hooks/UsePosts'
-import axios from "axios";
-import PostService from "./API/PostService";
-import Loader from "./componets/UI/Loader/Loader";
+import React from "react";
+import './styles/App.css'
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
-  const [posts, setPosts] = useState([])
-const [filter, setFilter] = useState({sort: '', query: ''})
-const [modal, setModal] = useState(false)
-const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-const [isPostsLoading, setIsPostsLoading] = useState(false)
-
-useEffect(() => {
-  fetchPosts()
-  }, [])
-
-const createPost = (newPost) => {
-  setPosts([...posts, newPost])
-  setModal(false)
-}
-
-const removePost = (post) => {
-  setPosts(posts.filter(p => p.id !== post.id))
-}
-
-async function fetchPosts() {
-  setIsPostsLoading(true)
-  setTimeout(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
-    setIsPostsLoading(false)
-  }, 1000)
-}
-
   return (
-    <div className="App">
-<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-  Создать пользователя
-</MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-      <PostForm create={createPost}/>
-       </MyModal>
-   <hr style={{margin: '15px 0'}}/>
-   <PostFilter
-   filter={filter}
-   setFilter={setFilter}
-   /> 
-{isPostsLoading
-  ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-  :<Postlist remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
+    <BrowserRouter>
+    <Route path='/about'>
+<About/>
+    </Route>
+    <Route path='/about'>
+<About/>
+    </Route>f
+    </BrowserRouter>
+  )
 }
-    </div>
 
-    
-  );
-}
 export default App;
